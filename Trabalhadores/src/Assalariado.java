7ce8f5ffde01abd956f00b53e85d624919f50c8d
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2022-04-04 22:12:38 -0300

    Add files via upload

---
+++ Trabalhadores/src/Assalariado.java
@@ -0,0 +1,24 @@
+public class Assalariado extends Empregado{
+  private double salario;
+  
+  public Assalariado(){
+    
+  }
+
+  public Assalariado(double salario, String nome, String sobreNome, String cpf){
+	  super(nome, sobreNome, cpf);
+	  this.salario = salario;
+  }
+  
+  public double getSalario(){
+	  return salario;
+  }
+  
+  public void setSalario(double salario){
+	  this.salario = salario;
+  }
+  
+  public double vencimento(){
+    return salario;
+  }
+}
